d84c757603bcfab0d9a2e7676b3254deaa152f5e
Luren <majorasliink@gmail.com>
2022-09-30 16:14:47 -0500

    Descarga en PDF

--- src/assets/js/Scripts-Ventana-de-pago-procesado.js
+++ src/assets/js/Scripts-Ventana-de-pago-procesado.js
@@ -1,4 +1,6 @@
-const open = document.getElementById( 'open'); 
+const open = document.getElementById( 'open'),
+    tarjetacontainer = document.querySelector('#tarjeta');
+
 const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
@@ -9,3 +11,85 @@ open.addEventListener('click', () => {
 // close.addEventListener('click', () => {
 //     modal_container.classList.remove('show');
 // });
+
+const selectDate = document.getElementById('expiracion');
+
+
+const rotateTarjeta = () => {
+	if(tarjetacontainer.classList.contains('active') ) {
+
+		tarjetacontainer.classList.remove('active')
+        selectDate.classList.add('rotateDate');
+
+	} else {
+
+        selectDate.classList.add('rotateDate');
+        selectDate.classList.remove('selectExpirDisp');
+
+    }
+}
+
+const estilosPrint = () => {
+
+    rotateTarjeta();
+
+    const form = document.getElementById('formulario-tarjeta'),
+    positionTarjeta = document.getElementById('backTarjeta'),
+    positionTarjetaFront = document.getElementById('delantera'),
+    tarjetaBackFront = document.getElementById('containerTarjeta');
+
+    form.style.display="none";
+    positionTarjeta.classList.add('rotateBack');
+    positionTarjeta.classList.add('toPrintBack');
+    positionTarjetaFront.classList.add('toPrintFront');
+    tarjetaBackFront.classList.add('FrontBackPrint');
+
+    document.body.style.overflowX="hidden";
+    document.body.style.overflowY="hidden";
+
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    btnDownload = document.getElementById('btnCrearPdf');
+
+    btnDownload.addEventListener('click', () => {
+
+        estilosPrint();
+
+        const elementConvert = document.body;
+
+        html2pdf(elementConvert, {
+            margin:0,
+            filename: 'documento.pdf',
+
+            image: {
+                type: 'jpg',
+                quality: 0.98
+            },
+            html2canvas: {
+                scale: 2,
+                letterRendering: true
+            },
+            jsPDF: {
+                unit:"mm",
+                format:"a4",
+                orientation: 'portrait'
+            }
+
+        })
+
+    });
+
+})
+
+
+
+
+
+
+
+
+
+
